b30dc9f0c1698cdfef32ee9765d25d0dfbb5be38
Jevgeni Vakker <bandem@icloud.com>
2023-06-23 12:39:36 +0200

    day 4 completed

---
+++ day-4/checkpoint-2.playground/Contents.swift
@@ -0,0 +1,11 @@
+import Cocoa
+
+let albums = ["Red", "Fearless", "Red"]
+
+print("Albums \(albums.count) pieces.")
+
+let albumsUnique = Set(albums)
+
+print("Albums \(albumsUnique.count) pieces.")
+
+
