c1c766d733d44b256621e0d9b24bd886ac44e902
Jevgeni Vakker <bandem@icloud.com>
2023-06-23 12:58:56 +0200

    day 5 completed

---
+++ day-5/multiple-conditions.playground/Contents.swift
@@ -0,0 +1,25 @@
+import Cocoa
+
+let userAge = 14
+let hasParentalConsent = true
+
+if userAge >= 18 || hasParentalConsent == true {
+    print("You can buy the game")
+}
+
+enum TransportOption {
+    case airplane, helicopter, bicycle, car, scooter
+}
+
+let transport = TransportOption.airplane
+
+if transport == .airplane || transport == .helicopter {
+    print("Let's fly!")
+} else if transport == .bicycle {
+    print("I hope there's a bike path…")
+} else if transport == .car {
+    print("Time to get stuck in traffic.")
+} else {
+    print("I'm going to hire a scooter now!")
+}
+
