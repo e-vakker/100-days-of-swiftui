2e92b383413c9f265e1b6aa5a1034c1f64ba33d4
Jevgeni Vakker <bandem@icloud.com>
2023-07-06 22:41:34 +0200

    project-3 init

---
+++ day-23-24-project-3/ViewsAndModifiers/ViewsAndModifiers/ViewsAndModifiersApp.swift
@@ -0,0 +1,17 @@
+//
+//  ViewsAndModifiersApp.swift
+//  ViewsAndModifiers
+//
+//  Created by Jevgeni Vakker on 06.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct ViewsAndModifiersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
