e1d0d55f864648e7083e9760c7f99e0f3352075e
Jevgeni Vakker <bandem@icloud.com>
2023-07-09 23:09:52 +0200

    day-26-28-project-4 init

---
+++ day-26-28-project-4/BetterRest/BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Jevgeni Vakker on 09.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
