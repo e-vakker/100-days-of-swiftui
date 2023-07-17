63d37b97fc21ffc76d697140b2dbb2169a0c6f3b
Jevgeni Vakker <bandem@icloud.com>
2023-07-17 12:35:27 +0200

    day-36 project 7 init

---
+++ day-36-38-project-7/iExpense/iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Jevgeni Vakker on 17.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
