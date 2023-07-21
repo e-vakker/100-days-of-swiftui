674f9249213b42ca73481f992011372b9eff8770
Jevgeni Vakker <bandem@icloud.com>
2023-07-21 14:18:29 +0200

    day 43 project 9 init

---
+++ day-39-42-project-8/Moonshot/Moonshot/GridLayout.swift
@@ -0,0 +1,65 @@
+//
+//  GridLayout.swift
+//  Moonshot
+//
+//  Created by Jevgeni Vakker on 20.07.2023.
+//
+
+import SwiftUI
+
+struct GridLayout: View {
+    let columns = [
+        GridItem(.adaptive(minimum: 150))
+    ]
+    
+    let astronauts: [String: Astronaut]
+    let missions: [Mission]
+    
+    var body: some View {
+        ScrollView {
+            LazyVGrid(columns: columns) {
+                ForEach(missions) { mission in
+                    NavigationLink {
+                        MissionView(mission: mission, astronauts: astronauts)
+                    } label: {
+                        VStack {
+                            Image(mission.image)
+                                .resizable()
+                                .scaledToFit()
+                                .frame(width: 100, height: 100)
+                            
+                            VStack {
+                                Text(mission.displayName)
+                                    .font(.headline)
+                                    .foregroundColor(.white)
+                                Text(mission.formattedLaunchDate)
+                                    .font(.caption)
+                                    .foregroundColor(.white.opacity(0.50))
+                            }
+                            .padding(.vertical)
+                            .frame(maxWidth: .infinity)
+                            .background(.lightBackground)
+                            .clipShape(RoundedRectangle(cornerRadius: 10))
+                            .overlay(
+                                RoundedRectangle(cornerRadius: 10)
+                                    .stroke(.lightBackground)
+                            )
+                        }
+                    }
+                }
+            }
+            .padding()
+            .padding([.horizontal, .bottom])
+        }
+    }
+}
+
+struct GridLayout_Previews: PreviewProvider {
+    static let missions: [Mission] = Bundle.main.decode("missions.json")
+    static let astronauts: [String: Astronaut] = Bundle.main.decode("astronauts.json")
+    
+    static var previews: some View {
+        GridLayout(astronauts: astronauts, missions: missions)
+            .preferredColorScheme(.dark)
+    }
+}
---
+++ day-43-46-project-9/Drawing/Drawing/DrawingApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrawingApp.swift
+//  Drawing
+//
+//  Created by Jevgeni Vakker on 21.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct DrawingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
