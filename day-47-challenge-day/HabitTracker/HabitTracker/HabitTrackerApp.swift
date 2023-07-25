27a60a5f8d4b0a9efce10d75c315f5a194e5eee6
Jevgeni Vakker <bandem@icloud.com>
2023-07-25 23:13:51 +0200

    day 47 challenge day init

---
+++ day-47-challenge-day/HabitTracker/HabitTracker/HabitTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  HabitTrackerApp.swift
+//  HabitTracker
+//
+//  Created by Jevgeni Vakker on 25.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct HabitTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
