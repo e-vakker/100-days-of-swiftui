dce9b417ee2f3391289a13cc725036c5f839799f
Jevgeni Vakker <bandem@icloud.com>
2023-07-30 22:07:08 +0200

    day 49 project 9 init

---
+++ day-49-52-project-10/CupcakeCorner/CupcakeCorner/CupcakeCornerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CupcakeCornerApp.swift
+//  CupcakeCorner
+//
+//  Created by Jevgeni Vakker on 30.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct CupcakeCornerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
