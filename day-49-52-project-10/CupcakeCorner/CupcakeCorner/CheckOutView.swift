36c17bcaf78940a92eb2dc76d2750e07520263e0
Jevgeni Vakker <bandem@icloud.com>
2023-07-30 23:32:30 +0200

    day 51 completed

---
+++ day-49-52-project-10/CupcakeCorner/CupcakeCorner/CheckOutView.swift
@@ -0,0 +1,41 @@
+//
+//  CheckOutView.swift
+//  CupcakeCorner
+//
+//  Created by Jevgeni Vakker on 30.07.2023.
+//
+
+import SwiftUI
+
+struct CheckOutView: View {
+    @ObservedObject var order: Order
+    
+    var body: some View {
+        ScrollView {
+            VStack {
+                AsyncImage(url: URL(string: "https://hws.dev/img/cupcakes@3x.jpg"), scale: 3) { image in
+                    image
+                        .resizable()
+                        .scaledToFit()
+                } placeholder: {
+                    ProgressView()
+                }
+                .frame(height: 233)
+                
+                Text("Your total is \(order.cost, format: .currency(code: "USD"))")
+                    .font(.title)
+                
+                Button("Place Order", action: { })
+                    .padding()
+            }
+        }
+        .navigationTitle("Check out")
+        .navigationBarTitleDisplayMode(.inline)
+    }
+}
+
+struct CheckOutView_Previews: PreviewProvider {
+    static var previews: some View {
+        CheckOutView(order: Order())
+    }
+}
