564f3129af129455dcecf644a08234c18b767052
Jevgeni Vakker <bandem@icloud.com>
2023-08-01 21:03:27 +0200

    day 53 project 11 init

---
+++ day-53-56-project-11/Bookworm/Bookworm/BookwormApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookwormApp.swift
+//  Bookworm
+//
+//  Created by Jevgeni Vakker on 01.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct BookwormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
