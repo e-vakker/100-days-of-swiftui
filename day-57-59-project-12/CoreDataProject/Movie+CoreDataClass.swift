ae752b93822fb503b21e77fdd6219a3768492ecc
Jevgeni Vakker <bandem@icloud.com>
2023-08-06 22:56:38 +0200

    day 57 completed

---
+++ day-57-59-project-12/CoreDataProject/Movie+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Movie+CoreDataClass.swift
+//  CoreDataProject
+//
+//  Created by Jevgeni Vakker on 06.08.2023.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Movie)
+public class Movie: NSManagedObject {
+
+}
