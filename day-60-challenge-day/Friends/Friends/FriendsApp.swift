f37fe8dcd8edb5b47b60a6542a0b309ba7528148
Jevgeni Vakker <bandem@icloud.com>
2023-08-09 21:09:49 +0200

    day 60 init

---
+++ day-60-challenge-day/Friends/Friends/FriendsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FriendsApp.swift
+//  Friends
+//
+//  Created by Jevgeni Vakker on 09.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct FriendsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
