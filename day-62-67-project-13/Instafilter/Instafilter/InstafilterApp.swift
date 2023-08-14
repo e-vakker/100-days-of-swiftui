8638284e3a37f1ad8c6e0492066a83cd0be9f294
Jevgeni Vakker <bandem@icloud.com>
2023-08-14 13:22:40 +0200

    day 62 project 12 init

---
+++ day-62-67-project-13/Instafilter/Instafilter/InstafilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstafilterApp.swift
+//  Instafilter
+//
+//  Created by Jevgeni Vakker on 14.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct InstafilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
