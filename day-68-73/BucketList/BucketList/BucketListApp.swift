ce4aefe5a34ce5cc820e20460ded6ebac6df0ad9
Jevgeni Vakker <bandem@icloud.com>
2023-08-21 12:26:47 +0200

    day 68 init

---
+++ day-68-73/BucketList/BucketList/BucketListApp.swift
@@ -0,0 +1,17 @@
+//
+//  BucketListApp.swift
+//  BucketList
+//
+//  Created by Jevgeni Vakker on 21.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct BucketListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
