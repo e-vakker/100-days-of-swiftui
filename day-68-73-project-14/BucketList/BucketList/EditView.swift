e61a18ff0ff26ccaf9feafb94743a65e24f96ea8
Jevgeni Vakker <bandem@icloud.com>
2023-08-24 01:13:57 +0200

    rename folder from day-68-73 to day-68-73-project-14

---
+++ day-68-73-project-14/BucketList/BucketList/EditView.swift
@@ -0,0 +1,65 @@
+//
+//  EditView.swift
+//  BucketList
+//
+//  Created by Jevgeni Vakker on 21.08.2023.
+//
+
+import SwiftUI
+
+struct EditView: View {
+    @StateObject private var viewModel: ViewModel
+    
+    @Environment(\.dismiss) var dismiss
+    
+    var onSave: (Location) -> Void
+    
+    init(location: Location, onSave: @escaping (Location) -> Void) {
+        self.onSave = onSave
+        _viewModel = StateObject(wrappedValue: ViewModel(location: location))
+    }
+    
+    var body: some View {
+        NavigationView {
+            Form {
+                Section {
+                    TextField("Place name", text: $viewModel.name)
+                    TextField("Description", text: $viewModel.description)
+                }
+                
+                Section("Nearby…") {
+                    switch viewModel.loadingState {
+                    case .loaded:
+                        ForEach(viewModel.pages, id: \.pageid) { page in
+                            Text(page.title)
+                                .font(.headline)
+                            + Text(": ") +
+                            Text(page.description)
+                                .italic()
+                        }
+                    case .loading:
+                        Text("Loading…")
+                    case .failed:
+                        Text("Please try again later.")
+                    }
+                }
+            }
+            .navigationTitle("Place details")
+            .toolbar {
+                Button("Save") {
+                    onSave(viewModel.newLocation())
+                    dismiss()
+                }
+            }
+            .task {
+                await viewModel.fetchNearbyPlaces()
+            }
+        }
+    }
+}
+
+struct EditView_Previews: PreviewProvider {
+    static var previews: some View {
+        EditView(location: Location.example) { newLocation in }
+    }
+}
