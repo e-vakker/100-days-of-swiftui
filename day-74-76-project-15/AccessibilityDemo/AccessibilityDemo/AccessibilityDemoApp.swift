bbefc6d669fdd22a55dfc852e146fa07aa7b742d
Jevgeni Vakker <bandem@icloud.com>
2023-08-24 21:07:23 +0200

    day 74 completed

---
+++ day-74-76-project-15/AccessibilityDemo/AccessibilityDemo/AccessibilityDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  AccessibilityDemoApp.swift
+//  AccessibilityDemo
+//
+//  Created by Jevgeni Vakker on 24.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct AccessibilityDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ day-74-76-project-15/AccessibilityDemo/AccessibilityDemo/ContentView.swift
@@ -0,0 +1,90 @@
+//
+//  ContentView.swift
+//  AccessibilityDemo
+//
+//  Created by Jevgeni Vakker on 24.08.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    let pictures = [
+        "ales-krivec-15949",
+        "galina-n-189483",
+        "kevin-horstmann-141705",
+        "nicolas-tissot-335096"
+    ]
+    
+    let labels = [
+        "Tulips",
+        "Frozen tree buds",
+        "Sunflowers",
+        "Fireworks"
+    ]
+    
+    @State private var selectedPicture = Int.random(in: 0...3)
+    
+    @State private var value = 10
+    
+    
+    var body: some View {
+        VStack {
+            Image(pictures[selectedPicture])
+                .resizable()
+                .scaledToFit()
+                .onTapGesture {
+                    selectedPicture = Int.random(in: 0...3)
+                }
+                .accessibilityLabel(labels[selectedPicture])
+                .accessibilityAddTraits(.isButton)
+                .accessibilityRemoveTraits(.isImage)
+            
+            VStack {
+                Text("Your score is")
+                Text("1000")
+                    .font(.title)
+            }
+            .accessibilityElement(children: .combine)
+            
+            VStack {
+                Text("Your score is")
+                Text("1000")
+                    .font(.title)
+            }
+            .accessibilityElement(children: .ignore)
+            .accessibilityLabel("Your score is 1000")
+            Spacer()
+            HStack {
+                Button("Decrement") {
+                    value -= 1
+                }
+                
+                Text("\(value)")
+                
+                Button("Increment") {
+                    value += 1
+                }
+            }
+            .accessibilityElement()
+            .accessibilityLabel("Value")
+            .accessibilityValue(String(value))
+            .accessibilityAdjustableAction { direction in
+                switch direction {
+                case .increment:
+                    value += 1
+                case .decrement:
+                    value -= 1
+                default:
+                    print("Not handled.")
+                }
+            }
+            
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
