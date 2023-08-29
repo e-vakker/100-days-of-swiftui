e7ed8810012aed3105c50d484a38b17efde3a0e9
Jevgeni Vakker <bandem@icloud.com>
2023-08-29 22:35:37 +0200

    day 77 init

---
+++ day-77-challenge-day/MemPix/MemPix/MemPixApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemPixApp.swift
+//  MemPix
+//
+//  Created by Jevgeni Vakker on 29.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct MemPixApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
