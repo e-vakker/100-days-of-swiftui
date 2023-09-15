d2ac23d563e2dd6310325b1a2836ae9a4ede1c15
Jevgeni Vakker <bandem@icloud.com>
2023-09-15 12:37:35 +0200

    day 84 completed

--- day-79-85-project-16/HotProspects/HotProspects/MeView.swift
+++ day-79-85-project-16/HotProspects/HotProspects/MeView.swift
@@ -12,6 +12,7 @@ import SwiftUI
 struct MeView: View {
     @State private var name = "Anonymous"
     @State private var email = "you@yoursite.com"
+    @State private var qrCode = UIImage()
     
     let context = CIContext()
     let filter = CIFilter.qrCodeGenerator()
@@ -27,14 +28,30 @@ struct MeView: View {
                     .textContentType(.emailAddress)
                     .font(.title)
                 
-                Image(uiImage: generateQRCode(from: "\(name)\n\(email)"))
+                Image(uiImage: qrCode)
                     .resizable()
                     .interpolation(.none)
                     .scaledToFit()
                     .frame(width: 200, height: 200)
+                    .contextMenu {
+                        Button {
+                            let imageSaver = ImageSaver()
+                            imageSaver.writeToPhotoAlbum(image: qrCode)
+                        } label: {
+                            Label("Save to photos", systemImage: "square.and.arrow.down")
+                        }
+                    }
             }
             .navigationTitle("Your code")
+            .onAppear(perform: updateCode)
+            .onChange(of: [name, email]) { _ in
+                updateCode()
+            }
         }
+    }
+    
+    func updateCode() {
+        qrCode = generateQRCode(from: "\(name)\n\(email)")
     }
     
     func generateQRCode(from string: String) -> UIImage {
@@ -42,7 +59,8 @@ struct MeView: View {
         
         if let outputImage = filter.outputImage {
             if let cgimg = context.createCGImage(outputImage, from: outputImage.extent) {
-                return UIImage(cgImage: cgimg)
+                let qrCode = UIImage(cgImage: cgimg)
+                return qrCode
             }
         }
         
--- day-79-85-project-16/HotProspects/HotProspects/Prospect.swift
+++ day-79-85-project-16/HotProspects/HotProspects/Prospect.swift
@@ -9,15 +9,36 @@ import Foundation
 
 
 @MainActor class Prospects: ObservableObject {
-    @Published var people: [Prospect]
+    @Published private(set) var people: [Prospect]
+    
+    let saveKey = "SavedData"
     
     init() {
-        self.people = []
+        if let data = UserDefaults.standard.data(forKey: saveKey) {
+            if let decoded = try? JSONDecoder().decode([Prospect].self, from: data) {
+                people = decoded
+                return
+            }
+        }
+        
+        people = []
     }
     
     func toggle(_ prospect: Prospect) {
         objectWillChange.send()
         prospect.isContacted.toggle()
+        save()
+    }
+    
+    func add(_ prospect: Prospect) {
+        people.append(prospect)
+        save()
+    }
+    
+    private func save() {
+        if let encoded = try? JSONEncoder().encode(people) {
+            UserDefaults.standard.set(encoded, forKey: saveKey)
+        }
     }
 }
 
