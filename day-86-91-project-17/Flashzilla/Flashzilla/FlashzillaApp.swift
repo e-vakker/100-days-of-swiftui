50acd2590c72636818fe5fb3dc5c676a57cf97ab
Jevgeni Vakker <bandem@icloud.com>
2023-09-19 12:35:30 +0200

    day 86 project init

---
+++ day-86-91-project-17/Flashzilla/Flashzilla/FlashzillaApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlashzillaApp.swift
+//  Flashzilla
+//
+//  Created by Jevgeni Vakker on 19.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct FlashzillaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
