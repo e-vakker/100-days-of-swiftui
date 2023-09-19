0c5ab955476a1c8be8bb139c7dab5bc2d767ab1c
Jevgeni Vakker <bandem@icloud.com>
2023-09-19 14:43:58 +0200

    day 86 completed

--- day-86-91-project-17/Flashzilla/Flashzilla/ContentView.swift
+++ day-86-91-project-17/Flashzilla/Flashzilla/ContentView.swift
@@ -8,19 +8,101 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var currentAmount = 0.0
+    @State private var finalAmount = 1.0
+    
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
             Text("Hello, world!")
+                .scaleEffect(finalAmount - currentAmount)
+                .gesture(MagnificationGesture()
+                    .onChanged { amount in
+                        currentAmount = amount - 1
+                    }
+                    .onEnded { amount in
+                        finalAmount += currentAmount
+                        currentAmount = 0
+                    })
         }
         .padding()
     }
 }
 
+struct ContentView2: View {
+    @State private var currentAmount = Angle.zero
+    @State private var finalAmount = Angle.zero
+    
+    
+    var body: some View {
+        VStack {
+            Text("Hello, world 2")
+                .rotationEffect(currentAmount + finalAmount)
+                .gesture(RotationGesture()
+                    .onChanged { angle in
+                        currentAmount = angle
+                    }
+                    .onEnded { angle in
+                        finalAmount += currentAmount
+                        currentAmount = .zero
+                    })
+        }
+        .padding()
+    }
+}
+
+struct ContentView3: View {
+    @State private var offset = CGSize.zero
+    @State private var isDragging = false
+
+    var body: some View {
+        let dragGesture = DragGesture()
+            .onChanged { value in offset = value.translation }
+            .onEnded { _ in
+                withAnimation {
+                    offset = .zero
+                    isDragging = false
+                }
+            }
+        
+        let pressGesture = LongPressGesture()
+            .onEnded { value in
+                withAnimation {
+                    isDragging = true
+                }
+            }
+        
+        let combined = pressGesture.sequenced(before: dragGesture)
+        
+        Circle()
+            .fill(.red)
+            .frame(width: 64, height: 64)
+            .scaleEffect(isDragging ? 1.5 : 1.0)
+            .offset(offset)
+            .gesture(combined)
+    }
+}
+
+struct ContentShapeTest: View {
+    
+    var body: some View {
+        VStack {
+            Text("Hello")
+            Spacer().frame(height: 100)
+            Text("World")
+        }
+        .contentShape(Rectangle())
+        .onTapGesture {
+            print("VStack tapped!")
+        }
+    }
+}
+
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
+        ContentShapeTest()
         ContentView()
+        ContentView2()
+        ContentView3()
     }
 }
