721a2edd06cde2214002601c0fc633ff35cbe237
Jevgeni Vakker <bandem@icloud.com>
2023-09-27 16:04:59 +0200

    Day 94: Completed

--- day-92-94-project-18/LayoutAndGeometry/LayoutAndGeometry/ContentView.swift
+++ day-92-94-project-18/LayoutAndGeometry/LayoutAndGeometry/ContentView.swift
@@ -8,16 +8,38 @@
 import SwiftUI
 
 struct ContentView: View {
+    let colors: [Color] = [.red, .green, .blue, .orange, .pink, .purple, .yellow]
+    
     var body: some View {
-        VStack(alignment: .leading) {
-            ForEach(0..<10) { position in
-                Text("Number \(position)")
-                    .alignmentGuide(.leading) { _ in CGFloat(position) * -10 }
+        GeometryReader { fullView in
+            ScrollView(.vertical) {
+                ForEach(0..<50) { index in
+                    GeometryReader { geo in
+                        let minY = geo.frame(in: .global).minY
+                        let fullViewHalf = fullView.size.height / 2
+                        
+                        let normalizedValueOpacity = min(max((minY - fullViewHalf) / (fullViewHalf) + 0.8, 0), 1)
+                        let normalizedValueScale = min(max((minY - fullViewHalf) / (fullViewHalf) + 0.9, 0.50), 1.20)
+                        
+                        let hue = Double(index) / Double(50)
+                        let color = Color(hue: hue, saturation: 1.0, brightness: 1.0)
+                        
+                        Text("Row #\(index)")
+                            .font(.title)
+                            .frame(maxWidth: .infinity)
+                            .background(color)
+                            .rotation3DEffect(.degrees(minY - fullViewHalf) / 5, axis: (x: 0, y: 1, z: 0))
+                            .opacity(normalizedValueOpacity)
+                            .scaleEffect(normalizedValueScale)
+                            .onAppear {
+                                print(normalizedValueScale)
+                            }
+                    }
+                    .frame(height: 40)
+                }
             }
         }
-        .background(.red)
-        .frame(width: 400, height: 400)
-        .background(.blue)
+        
     }
 }
 
