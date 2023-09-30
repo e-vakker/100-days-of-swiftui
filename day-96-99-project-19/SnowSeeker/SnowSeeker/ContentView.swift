4285484eeabad1df618a551db2c3bae5edd026bf
Jevgeni Vakker <bandem@icloud.com>
2023-09-30 18:04:51 +0200

    Day 96: Completed

--- day-96-99-project-19/SnowSeeker/SnowSeeker/ContentView.swift
+++ day-96-99-project-19/SnowSeeker/SnowSeeker/ContentView.swift
@@ -7,15 +7,41 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct User: Identifiable {
+    var id = "Taylor Swift"
+}
+
+struct UserView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        Group {
+            Text("Name: Paul")
+            Text("Country: England")
+            Text("Pets: Luna and Arya")
         }
-        .padding()
+        .font(.title)
+    }
+}
+
+struct ContentView: View {
+    @State private var searchText = ""
+    let allNames = ["Subh", "Vina", "Melvin", "Stefanie"]
+    
+    var body: some View {
+        NavigationView {
+            List(filteredNames, id: \.self) { name in
+                Text(name)
+            }
+            .searchable(text: $searchText, prompt: "Look for something")
+            .navigationTitle("Searching")
+        }
+    }
+    
+    var filteredNames: [String] {
+        if searchText.isEmpty {
+            return allNames
+        } else {
+            return allNames.filter { $0.contains(searchText) }
+        }
     }
 }
 
