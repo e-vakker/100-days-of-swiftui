fc3cb179fd975b1a1c339a5899a0964c5951c2ed
Jevgeni Vakker <bandem@icloud.com>
2023-09-30 17:25:32 +0200

    Day 96: Init.

---
+++ day-96-99-project-19/SnowSeeker/SnowSeeker/SnowSeekerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnowSeekerApp.swift
+//  SnowSeeker
+//
+//  Created by Jevgeni Vakker on 30.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct SnowSeekerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
